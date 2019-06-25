7de32821313753097302ccb0c28b7ab4a5f08574
chenzheng <baight.chen@ximalaya.com>
2019-06-25 10:36:00 +0800

    cdz-icon开发

--- cdz-icon/cdz-icon.component.ts
+++ cdz-icon/cdz-icon.component.ts
@@ -5,37 +5,22 @@ import { ElementRef, Renderer, HostListener, Input, ViewChild } from '@angular/c
   templateUrl: './cdz-icon.component.html',
   styleUrls: ['./cdz-icon.component.css']
 })
-export class CDZIconComponent implements OnInit, OnChanges {
+export class CDZIconComponent implements OnInit {
 
   private _src: string
   @Input() set src(src: string){
     this._src = src
     let value = "url('" + src + "')"
-    console.log(value)
-    this.renderer.setElementStyle(this.host.nativeElement, "mask-image", "url(/assets/icon/power.png)")
-    this.renderer.setElementStyle(this.host.nativeElement, "-webkit-mask-image", "url(/assets/icon/power.png)")
+    this.renderer.setElementStyle(this.host.nativeElement, "mask-image", value)
+    this.renderer.setElementStyle(this.host.nativeElement, "-webkit-mask-image", value)
   }
   get src(){
     return this._src
   }
 
-  private _color: string
-  @Input() set color(color: string){
-    this._color = color
-    this.renderer.setElementStyle(this.host.nativeElement, "background-color", color)
-  }
-  get color(){
-    return this._color
-  }
-
   constructor(private renderer: Renderer, private host:ElementRef) { 
-
   }
 
   ngOnInit(){
-  }
-
-  ngOnChanges(changes: SimpleChanges) {
-    console.log(this.host.nativeElement.class)
   }
 }
